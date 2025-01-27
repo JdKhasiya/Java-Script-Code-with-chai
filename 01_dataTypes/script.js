81560dcc8acc0493ac1078e3f1f11d8bad1ade30
JdKhasiya <129826758+JdKhasiya@users.noreply.github.com>
2025-01-27 01:51:03 +0000

    container config done and 01 data types

---
+++ 01_dataTypes/script.js
@@ -0,0 +1,23 @@
+"use script"; //treat all JS code as newer version
+
+// alert(3+3)  in doc this cant work directly as we are using Node JS not brouser
+
+console.log(3
+    +3 //code readability is not good in this
+)
+
+// two documentations tc39 and mdn for JS
+
+let name = "jaydip"
+let age  =20
+let bool = true
+
+// primtitve DT
+// number => 2 to power 53 nearly, bigInt, string => "", boolean =>true/false
+//  null( its also an standalone value), undefined, symbol (to find uniqueness)  
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
